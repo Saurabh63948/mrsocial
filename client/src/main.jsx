d601c107b845515c09e1aa25b129aab8d67ed011
Admin <Admin@DESKTOP-4NC3IP9>
2025-03-29 14:44:06 +0530

    Fixed authContext import issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthContextProvider } from "./context/authContext";
-import { DarkModeContextProvider } from "./context/darkModeContext";
+import { AuthContextProvider } from "../src/context/AuthContext";
+import { DarkModeContextProvider } from "../src/context/DarkModeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
